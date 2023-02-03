4a908694989ed7a90d3111dc46bc01148d4c0c61
Michał Garapich <michal@garapich.pl>
2023-02-03 02:09:47 +0100

    request-notification-permission dev

--- src/lib/queue/components/request-notification-permission.svelte
+++ src/lib/queue/components/request-notification-permission.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
   import Button from '$lib/shared/components/button.svelte';
-  import { IconNotification } from '@tabler/icons-svelte';
+  import { IconNotification, IconCircleX } from '@tabler/icons-svelte';
+  import { slide } from 'svelte/transition';
 
   let permission = Notification.permission;
+
+  const requestPermission = () => {
+    Notification.requestPermission().then(userPermission => (permission = userPermission));
+  };
 </script>
 
 {#if permission === 'default'}
-  <div class="flex flex-col bg-amber-200 md:flex-row">
+  <div class="flex flex-col bg-amber-200 md:flex-row" out:slide>
     <div class="flex grow flex-col items-center gap-2 py-2 px-4 text-lg text-black md:flex-row">
       <div class="hidden md:inline-block"><IconNotification size={24} /></div>
       <span
@@ -15,8 +20,18 @@
       >
     </div>
 
-    <button class="self-stretch">
+    <button class="self-stretch" on:click={requestPermission}>
       <Button alternateColor="rgb(10 26 51)" className="h-full">Allow notifications</Button>
     </button>
+  </div>
+{:else if permission === 'denied'}
+  <div
+    class="flex flex-col items-center gap-2 bg-rose-700 py-2 px-4 text-lg text-white md:flex-row"
+  >
+    <div class="hidden md:inline-block"><IconCircleX size={24} /></div>
+    <span
+      >You have disabled browser notifications. You will not be warned when the match is about to
+      start.</span
+    >
   </div>
 {/if}
