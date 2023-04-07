a07d6ee994e8d63036605314b738ec9361d90070
Michał Garapich <michal@garapich.pl>
2023-04-08 01:32:58 +0200

    fix footer links

--- src/lib/core/components/footer.svelte
+++ src/lib/core/components/footer.svelte
@@ -4,12 +4,22 @@
       <span>© 2023 tf2pickup.org | version 4.0.0 alpha.7</span>
       <div class="grow" />
       <div class="flex flex-row gap-5">
-        <a href="/privacy-policy">Privacy policy</a>
-        <a href="https://github.com/tf2pickup-org" target="_blank">Github</a>
-        <a href="https://github.com/tf2pickup-org/client/blob/master/CHANGELOG.md" target="_blank"
-          >Changelog</a
+        <a href="/privacy-policy" class="link">Privacy policy</a>
+        <a href="https://github.com/tf2pickup-org" target="_blank" class="link">Github</a>
+        <a
+          href="https://github.com/tf2pickup-org/client/blob/master/CHANGELOG.md"
+          class="link"
+          target="_blank">Changelog</a
         >
       </div>
     </div>
   </div>
 </footer>
+
+<style lang="scss">
+  .link {
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+</style>
