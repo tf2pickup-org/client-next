452c97a75b208b8f8ac1e1d6f511ea26bd9300b4
Michał Garapich <michal@garapich.pl>
2023-09-19 14:58:30 +0200

    fix footer on mobile

--- src/lib/core/components/footer.svelte
+++ src/lib/core/components/footer.svelte
@@ -1,9 +1,11 @@
-<footer class="my-4 w-full">
-  <div class="container mx-auto">
-    <div class="text-abru-light-75 flex flex-row text-sm font-normal">
-      <span>© 2023 tf2pickup.org | version 4.0.0 alpha.7</span>
+<footer class="w-full">
+  <div class="container mx-auto my-4">
+    <div
+      class="text-abru-light-75 flex flex-col items-center gap-2 text-sm font-normal md:flex-row md:gap-0"
+    >
+      <span>© 2023 tf2pickup.org | version 4.0.0-alpha.7</span>
       <div class="grow" />
-      <div class="flex flex-row gap-5">
+      <div class="flex flex-col items-center gap-2 md:flex-row md:gap-5">
         <a href="/privacy-policy" class="link">Privacy policy</a>
         <a href="https://github.com/tf2pickup-org" target="_blank" class="link">Github</a>
         <a
