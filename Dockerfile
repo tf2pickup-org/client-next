1e59004bb6a368b394f3ebdbb47d0172b11cf2f2
Michał Garapich <michal@garapich.pl>
2024-02-27 10:15:19 +0100

    feat(build): add server.js

--- Dockerfile
+++ Dockerfile
@@ -17,5 +17,5 @@ FROM base
 COPY --from=prod-deps /app/node_modules /app/node_modules
 COPY --from=build /app/build /app/build
 USER node
-CMD [ "node", "build/index.js" ]
+CMD [ "node", "server.js" ]
 EXPOSE 3000
---
+++ server.js
@@ -0,0 +1,38 @@
+import { handler } from './build/handler.js';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import polka from 'polka';
+
+const backendUri = process.env.BACKEND_URL || 'http://localhost:3000';
+
+console.log(`Proxying requests to ${backendUri}`);
+
+const api = createProxyMiddleware({
+  target: backendUri,
+  changeOrigin: true,
+  cookieDomainRewrite: {
+    'api.tf2pickup.pl': 'tf2pickup.pl',
+  },
+  pathRewrite: {
+    '^/api': '',
+  },
+});
+
+const socketIo = createProxyMiddleware({
+  target: backendUri,
+  changeOrigin: true,
+  ws: true,
+});
+
+polka()
+  .use('/api', api)
+  .use('/socket.io', socketIo)
+  .use((req, res, next) => {
+    if (req.url.startsWith('/api') || req.url.startsWith('/socket.io')) {
+      return next();
+    }
+
+    return handler(req, res, next);
+  })
+  .listen(4000, () => {
+    console.log(`> Running on 3000`);
+  });
