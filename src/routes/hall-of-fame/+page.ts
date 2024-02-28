9128bff966c49a53ab4a081dda490bc97a596404
Michał Garapich <michal@garapich.pl>
2024-02-28 12:11:41 +0100

    fix(hall-of-fame): fix hof loading

--- src/routes/hall-of-fame/+page.ts
+++ src/routes/hall-of-fame/+page.ts
@@ -5,13 +5,13 @@ import type { PageLoad } from './$types';
 export const load = (async ({ fetch }) => {
   const hallOfFame = await fetchHallOfFame(fetch);
   const [mostActivePlayers, mostActiveMedics] = [
-    Promise.all(
+    await Promise.all(
       hallOfFame.mostActivePlayers.map(async r => ({
         count: r.count,
         player: await fetchPlayer(r.player),
       })),
     ),
-    Promise.all(
+    await Promise.all(
       hallOfFame.mostActiveMedics.map(async r => ({
         count: r.count,
         player: await fetchPlayer(r.player),
